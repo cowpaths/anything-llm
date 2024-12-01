e41b1c0543c7cf1656792fdf9ac97c10b9e436ea
Steven Guitar <steven@fullstory.com>
2024-12-01 00:35:41 -0500

    try to set custom response headers

--- server/index.js
+++ server/index.js
@@ -77,6 +77,8 @@ if (process.env.NODE_ENV !== "development") {
         // Disable I-framing of entire site UI
         res.removeHeader("X-Powered-By");
         res.setHeader("X-Frame-Options", "DENY");
+        res.setHeader("Cache-Control", "no-store");
+        res.setHeader("X-Cowpaths", "true");
       },
     })
   );
