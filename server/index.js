fac6a8f84934b1fd6a558f921c12349f7a48c569
Steven Guitar <steven@fullstory.com>
2024-12-06 23:35:45 -0500

    forced build to test LB changes

--- server/index.js
+++ server/index.js
@@ -78,7 +78,7 @@ if (process.env.NODE_ENV !== "development") {
         res.removeHeader("X-Powered-By");
         res.setHeader("X-Frame-Options", "DENY");
         // res.setHeader("Cache-Control", "no-store");
-        res.setHeader("X-Cowpaths", "true");
+        res.setHeader("X-Cowpaths", "yoda");
         if (path.endsWith("index.js")) {
           res.setHeader("Cache-Control", "no-store");
         }
